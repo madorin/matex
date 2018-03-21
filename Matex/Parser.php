1fd0f9ad7e561981ce16b5965a609de719fc3ac7
Dorin Marcoci <dorin.marcoci@marcodor.com>
2018-03-21 11:04:51 +0200

    camelCase, optimizations, string arguments Now functions may have strings as arguments, like: FieldValue('amount')

--- Matex/Parser.php
+++ Matex/Parser.php
@@ -6,194 +6,172 @@ class Exception extends \Exception {}
 
 class Parser {
 
-private $Pos;
-private $Text;
+private $pos;
+private $text;
 
-public $Variables = [];
-public $OnVariable;
-public $Functions = []; // ARCTAN COS SIN TAN ABS EXP LN LOG SQRT SQR INT FRAC TRUNC ROUND ARCSIN ARCCOS SIGN NOT
-public $OnFunction;
+public $variables = [];
+public $onVariable;
+public $functions = []; // ARCTAN COS SIN TAN ABS EXP LN LOG SQRT SQR INT FRAC TRUNC ROUND ARCSIN ARCCOS SIGN NOT
+public $onFunction;
 
-private function GetNumber(float &$Value = null): bool {
-	if (!ctype_digit($Char = $this->Text[$this->Pos] ?? false))
-		return false;
-	$OldPos = $this->Pos;
-	$WasDec = false;
-	do {
-		if ($Char == '.') {
-			if ($WasDec) {
-				$Result = false;
-				break;
-			}
-			$WasDec = true;
-		}
-		$this->Pos++;
-	} while (ctype_digit($Char = $this->Text[$this->Pos] ?? false) || ($Char == '.'));
-	if (isset($Result) || !in_array($Char, [false, '+', '-', '/', '*', '^', ')'])) {
-		$this->Pos = $OldPos;
-		return false;
+private function getIdentity(bool &$kind = null, string &$value = null): bool {
+	$ops = $this->pos;
+	$ist = ($this->text[$this->pos] ?? false) == '\'';
+	if ($ist) $this->pos++;
+	while (($char = $this->text[$this->pos] ?? false) && (($ist && ($char != '\'')) || ctype_alnum($char) || in_array($char, ['_', '.'])))
+		$this->pos++;
+	if (!$len = $this->pos - $ops) return false;
+	$str = substr($this->text, $ops, $len);
+	if ($ist) {
+		if ($char != '\'') return false;
+		$this->pos++;
+		$value = substr($str, 1);
+		$kind = 4;
+		return true;
 	}
-	$Value = (float) substr($this->Text, $OldPos, $this->Pos - $OldPos);
+	if (is_numeric($str)) $kind = 1;
+	else {
+		if (ctype_digit($str[0]) || (strpos($str, '.') !== false)) return false;
+		$kind = $char == '(' ? 3 : 2;
+	}
+	$value = $str;
 	return true;
 }
 
-private function GetIdentity(bool &$Kind = null, string &$Name = null): bool {
-	if (!(ctype_alpha($Char = $this->Text[$this->Pos] ?? false) || ($Char == '_')))
-		return false;
-	$OldPos = $this->Pos;
-	do {
-		$this->Pos++;
-	} while (ctype_alnum($Char = $this->Text[$this->Pos] ?? false) || ($Char == '_'));
-	if (!in_array($Char, [false, '+', '-', '/', '*', '^', '(', ')'])) {
-		$this->Pos = $OldPos;
-		return false;
+private function getVariable(string $name): float {
+	$value = $this->variables[$name] ?? null;
+	if (!isset($value) && isset($this->onVariable)) {
+		call_user_func_array($this->onVariable, [$name, &$value]);
+		$this->variables[$name] = $value;
 	}
-	$Kind = ($Char != '(');
-	$Name = substr($this->Text, $OldPos, $this->Pos - $OldPos);
-	return true;
+	if (!isset($value))
+		throw new Exception('Unknown variable: '.$name, 5);
+	return $value;
 }
 
-private function ProVariable(string $Name, float &$Value = null): void {
-	if (is_numeric($Value = $this->Variables[$Name] ?? false))
-		return;
-	if (isset($this->OnVariable))
-		call_user_func_array($this->OnVariable, [$Name, &$Value]);
-	if (!is_numeric($Value))
-		throw new Exception('Unknown variable: '.$Name, 5);
-	$this->Variables[$Name] = $Value;
-}
-
-private function AddArgument(&$Arguments, $Argument) {
-	if ($Argument == '')
+private function addArgument(&$arguments, $argument) {
+	if ($argument == '')
 		throw new Exception('Empty argument', 4);
-	$Arguments[] = $Argument;
+	$arguments[] = $argument;
 }
 
-private function GetArguments(&$Arguments = []): bool {
-	$B = 1;
-	$this->Pos++;
-	$Mark = $this->Pos;
-	while ((($Char = $this->Text[$this->Pos] ?? false) !== false) && ($B > 0)) {
-		if (($Char == ',') && ($B == 1)) {
-			$this->AddArgument($Arguments, substr($this->Text, $Mark, $this->Pos - $Mark));
-			$Mark = $this->Pos + 1;
+private function getArguments(&$arguments = []): bool {
+	$b = 1;
+	$this->pos++;
+	$mark = $this->pos;
+	while ((($char = $this->text[$this->pos] ?? false) !== false) && ($b > 0)) {
+		if (($char == ',') && ($b == 1)) {
+			$this->addArgument($arguments, substr($this->text, $mark, $this->pos - $mark));
+			$mark = $this->pos + 1;
 		}
-		elseif ($Char == ')') $B--;
-		elseif ($Char == '(') $B++;
-		$this->Pos++;
+		elseif ($char == ')') $b--;
+		elseif ($char == '(') $b++;
+		$this->pos++;
 	}
-	if (!in_array($Char, [false, '+', '-', '/', '*', '^', ')']))
+	if (!in_array($char, [false, '+', '-', '/', '*', '^', ')']))
 		return false;
-	$this->AddArgument($Arguments, substr($this->Text, $Mark, $this->Pos - $Mark - 1));
+	$this->addArgument($arguments, substr($this->text, $mark, $this->pos - $mark - 1));
 	return true;
 }
 
-private function ProArgument($Arguments) {
-	$OPos = $this->Pos;
-	$OText = $this->Text;
-	$Result = [];
-	foreach ($Arguments as $Argument)
-		$Result[] = $this->Perform($Argument);
-	$this->Pos = $OPos;
-	$this->Text = $OText;
-	return $Result;
+private function proArguments($arguments) {
+	$ops = $this->pos;
+	$otx = $this->text;
+	$result = [];
+	foreach ($arguments as $argument)
+		$result[] = $this->perform($argument);
+	$this->pos = $ops;
+	$this->text = $otx;
+	return $result;
 }
 
-private function ProFunction(string $Name, float &$Value = null): void {
-	if (!isset($this->Functions[$Name])) {
-		if (isset($this->OnFunction))
-			call_user_func_array($this->OnFunction, [$Name, &$Function]);
-		if (!isset($Function))
-			throw new Exception('Unknown function: '.$Name, 6);
-		$this->Functions[$Name] = $Function;
+private function getFunction(string $name): float {
+	$routine = $this->functions[$name] ?? null;
+	if (!isset($routine) && isset($this->onFunction)) {
+		call_user_func_array($this->onFunction, [$name, &$routine]);
+		$this->functions[$name] = $routine;
 	}
-	else
-		$Function = $this->Functions[$Name];
-	if (!$this->GetArguments($Arguments))
+	if (!isset($routine))
+		throw new Exception('Unknown function: '.$name, 6);
+	if (!$this->getArguments($arguments))
 		throw new Exception('Syntax error', 1);
-	if (isset($Function['arc'])) {
-		if ($Function['arc'] != count($Arguments))
-			throw new Exception('Invalid argument count', 3);
-		$Value = call_user_func_array($Function['ref'], $this->ProArgument($Arguments));
-	}
-	else
-		$Value = call_user_func($Function['ref'], $this->ProArgument($Arguments));
+	if (isset($routine['arc']) && ($routine['arc'] != count($arguments)))
+		throw new Exception('Invalid argument count', 3);
+	return call_user_func_array($routine['ref'], $this->proArguments($arguments));
 }
 
-private function Term(): float {
-	if ($this->Text[$this->Pos] == '(') {
-		$this->Pos++;
-		$Value = $this->Calculate();
-		$this->Pos++;
-		if (!in_array($this->Text[$this->Pos] ?? false, [false, '+', '-', '/', '*', '^', ')']))
+private function term() {
+	if ($this->text[$this->pos] == '(') {
+		$this->pos++;
+		$value = $this->calculate();
+		$this->pos++;
+		if (!in_array($this->text[$this->pos] ?? false, [false, '+', '-', '/', '*', '^', ')']))
 			throw new Exception('Syntax error', 1);
-		return $Value;
+		return $value;
 	}
-	if (!$this->GetNumber($Value))
-		if ($this->GetIdentity($Kind, $Name)) {
-			if ($Kind)
-				$this->ProVariable($Name, $Value);
-			else
-				$this->ProFunction($Name, $Value);
-		}
-		else
-			throw new Exception('Syntax error', 1);
-	return $Value;
+	if (!$this->getIdentity($kind, $name))
+		throw new Exception('Syntax error', 1);
+	switch ($kind) {
+		case 1: return (float) $name;
+		case 2: return $this->getVariable($name);
+		case 3: return $this->getFunction($name);
+		case 4: return $name; // string
+	}
 }
 
-private function SubTerm(): float {
-	$Value = $this->Term();
-	while (in_array($Char = $this->Text[$this->Pos] ?? false, ['*', '^', '/'])) {
-		$this->Pos++;
-		$Term = $this->Term();
-		switch ($Char) {
+private function subTerm() {
+	$value = $this->term();
+	while (in_array($char = $this->text[$this->pos] ?? false, ['*', '^', '/'])) {
+		$this->pos++;
+		$term = $this->term();
+		switch ($char) {
 			case '*':
-				$Value = $Value * $Term;
+				$value = $value * $term;
 				break;
 			case '/':
-				if ($Term == 0)
+				if ($term == 0)
 					throw new Exception('Division by zero', 7);
-				$Value = $Value / $Term;
+				$value = $value / $term;
 				break;
 			case '^':
-				$Value = pow($Value, $Term);
+				$value = pow($value, $term);
 				break;
 		}
 	}
-	return $Value;
+	return $value;
 }
 
-private function Calculate(): float {
-	$Value = $this->SubTerm();
-	while (in_array($Char = $this->Text[$this->Pos] ?? false, ['+', '-'])) {
-		$this->Pos++;
-		$SubTerm = $this->SubTerm();
-		if ($Char == '-')
-			$SubTerm = -$SubTerm;
-		$Value += $SubTerm;
+private function calculate() {
+	$value = $this->subTerm();
+	while (in_array($char = $this->text[$this->pos] ?? false, ['+', '-'])) {
+		$this->pos++;
+		$subTerm = $this->subTerm();
+		if ($char == '-')
+			$subTerm = -$subTerm;
+		$value += $subTerm;
 	}
-	return $Value;
+	return $value;
 }
 
-private function Perform(string $Formula): float {
-	$this->Pos = 0;
-	if (in_array($Formula[0], ['-', '+']))
-		$Formula = '0'.$Formula;
-	$this->Text = $Formula;
-	return $this->Calculate();
+private function perform(string $formula) {
+	$this->pos = 0;
+	if (in_array($formula[0], ['-', '+']))
+		$formula = '0'.$formula;
+	$this->text = $formula;
+	return $this->calculate();
 }
 
-public function Execute(string $Formula): float {
-	$B = 0;
-	for ($I = 0; $I < strlen($Formula); $I++) {
-		switch ($Formula[$I]) {
-			case '(': $B++; break;
-			case ')': $B--; break;
+public function execute(string $formula): float {
+	$b = 0;
+	for ($i = 0; $i < strlen($formula); $i++) {
+		switch ($formula[$i]) {
+			case '(': $b++; break;
+			case ')': $b--; break;
 		}
 	}
-	if ($B != 0)
+	if ($b != 0)
 		throw new Exception('Unmatched brackets', 2);
-	return $this->Perform(str_replace(' ', '', strtolower($Formula)));
+	return $this->perform(str_replace(' ', '', strtolower($formula)));
 }
 
 }
